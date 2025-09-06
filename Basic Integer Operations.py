1db70f8b9249dc3a90d85556740d9e38bba7cbad
MrrMylo <camdavies2011@gmail.com>
2025-09-06 10:41:21 +0100

    Add files via upload

---
+++ Basic Integer Operations.py
@@ -0,0 +1,13 @@
+# Program title: Basic Integer operations
+
+num1 = int(input("Enter the first integer: "))
+num2 = int(input("Enter the second integer: "))
+
+# Print all completed equations
+
+print("Sum: " + str(num1 + num2))
+print("Difference: " + str(num1 - num2))
+print("Product: " + str(num1 * num2))
+print("Division: " + str(num1 / num2))
+print("Quotient: " + str(num1 // num2))
+print("Remainder: " + str(num1 % num2))
