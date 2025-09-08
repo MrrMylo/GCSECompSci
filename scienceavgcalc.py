e660cc80010a5a8346594af2e54796e0737f9b45
MrrMylo <camdavies2011@gmail.com>
2025-09-08 09:59:13 +0100

    Create scienceavgcalc.py

---
+++ scienceavgcalc.py
@@ -0,0 +1,6 @@
+# Science Department Average Calculator
+bio = int(input("Enter the Biology Result: "))
+chem = int(input("Enter the Chemistry Result: "))
+physics = int(input("Enter the Physics Result: "))
+
+print("Student Average:" + str((bio + chem + physics)/ 3) + "%")
