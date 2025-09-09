d9316eb3174b6347a4a8972825d4daf8fda3806c
MrrMylo <camdavies2011@gmail.com>
2025-09-09 12:36:35 +0100

    added comments

--- Wage & Tax Calculator.py
+++ Wage & Tax Calculator.py
@@ -1,9 +1,10 @@
-wage = (int(input("Enter Wage: ")))
+# Exaple of a selection statement
+wage = (int(input("Enter Wage: "))) # <--- This is an integer assignment
 
-if wage > 10000 and wage < 30000:
-    tax = wage * 0.25
+if wage > 10000 and wage < 30000: #<--- This is a complex condition
+    tax = wage * 0.25 #<--- True Path
 elif wage >= 30000:
-    tax = wage * 0.40
+    tax = wage * 0.40 #<--- False Path
 
 takehome = wage - tax
 print("Wage after tax: £" + str(takehome))
