78771abb45d377357dfb440669a2294d0b34cdc1
MrrMylo <camdavies2011@gmail.com>
2025-09-09 12:30:41 +0100

    Added Integer Input to the Persons Age

--- School Age.py
+++ School Age.py
@@ -1,7 +1,7 @@
 # School Age
-age = 4
+age = (int(input("Enter Age of Person: "))) #<-- This is an integer assignment
 
-if age > 5:
-    print("You are of school age.")
+if age > 5 and age < 18: #<--- This is a complex condition
+    print("You are of school age.") #<---- True Path
 else:
-    print("You are not of school age.")
+    print("You are not of school age.") #<--- False Path
