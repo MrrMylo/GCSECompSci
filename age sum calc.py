8fcb5887a7bf1c6a5b664e6f6d6525069e22e8f1
MrrMylo <camdavies2011@gmail.com>
2025-09-10 17:25:26 +0100

    edit to cover all

--- age sum calc.py
+++ age sum calc.py
@@ -1,7 +1,8 @@
 age1 = 11
 age2 = 14
-
-print("Age 1: " + str(age1))
-print("Age 2: " + str(age2))
+name1 = input("Enter name 1: ")
+name2 = input("Enter name 2: ")
+print(name1 + "Age: " + str(age1))
+print(name2 + "Age 2: " + str(age2))
 print("Sum of ages: " + str(age1 + age2))
 print("Average age: " + str((age1 + age2) / 2))
