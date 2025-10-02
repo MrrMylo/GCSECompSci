622720a70dd83d5f24851a449e5b4d6d2ea164ad
MrrMylo <camdavies2011@gmail.com>
2025-10-02 08:00:19 +0100

    missed work

---
+++ rgbhex.py
@@ -0,0 +1,7 @@
+red = input("Enter the Hex value for Red (e.g., A4): ")
+green = input("Enter the Hex value for Green (e.g., 4B): ")
+blue = input("Enter the Hex value for Blue (e.g., 82): ")
+
+hex_code = "#" + red + green + blue
+
+print("Your RGB code is " + hex_code)
