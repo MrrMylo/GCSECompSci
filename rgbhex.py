0d7315b687e522069db4b9dcc0e1b883134e4f08
MrrMylo <camdavies2011@gmail.com>
2025-10-28 11:37:58 +0000

    new files

--- rgbhex.py
+++ rgbhex.py
@@ -1,12 +1,2 @@
-text = input("Input Text You would like to Cypher: ")
-
-if not text.isupper():
-    print("Please only use Capital Letters.")
-else:
-    shift = int(input("How much would you like to shift by?: "))
-    result = ""
-
-    for c in text:
-        result = result + chr((ord(c) - 65 + shift) % 26 + 65)
-
-    print(result)
+file = input("Enter your file name: ").lower()
+print(file + str(".html"))
