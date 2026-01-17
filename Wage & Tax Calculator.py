1d712018b3fcce5331f4bb601546211471a39496
MrrMylo <camdavies2011@gmail.com>
2026-01-17 10:42:39 +0000

    changed tax bands in calculator - new files

--- Wage & Tax Calculator.py
+++ Wage & Tax Calculator.py
@@ -1,13 +1,28 @@
-# Exaple of a selection statement
-wage = (int(input("Enter Wage: "))) # <--- This is an integer assignment
+wage = (int(input("Enter Wage: £ "))) 
 tax = 0
-if wage >= 10000 and wage <= 30000: #<--- This is a complex condition
-    tax = wage * 0.25 #<--- True Path
+if wage >= 12571 and wage <= 15397: 
+    tax = wage * 0.19 
     takehome = wage - tax
     print("Wage after tax: £" + str(takehome))
-elif wage >= 30000:
-    tax = wage * 0.48 #<--- False Path
+elif wage >= 15398 and wage <= 27491:
+    tax = wage * 0.20 
+    takehome = wage - tax
+    print("Wage after tax: £" + str(takehome))
+elif wage >= 27492 and wage <= 43662: 
+    tax = wage * 0.21 
+    takehome = wage - tax
+    print("Wage after tax: £" + str(takehome))
+elif wage >= 43663 and wage <= 75000: 
+    tax = wage * 0.42 
+    takehome = wage - tax
+    print("Wage after tax: £" + str(takehome))
+elif wage >= 75001 and wage <= 125140: 
+    tax = wage * 0.45 
+    takehome = wage - tax
+    print("Wage after tax: £" + str(takehome))
+elif wage >= 125141: 
+    tax = wage * 0.48 
     takehome = wage - tax
     print("Wage after tax: £" + str(takehome))
 elif wage < 12570:
-    print("No tax to pay") #<--- False Path
+    print("No tax to pay. Within personal allowance") 
